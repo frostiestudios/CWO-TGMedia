242cca6b242c6746e7c2e820fd7b171038d3320a
thememetsar <mattwiner04@gmail.com>
2023-05-04 11:28:58 -0700

    0.2

---
+++ app.py
@@ -0,0 +1,19 @@
+from bottle import route,static_file,run,template
+import sqlite3
+@route('/')
+def index():
+   return static_file('index.html',root='')
+
+@route('/clients')
+def clients():
+   conn = sqlite3.connect('data.db')
+   c = conn.cursor()
+   c.execute("SELECT id, sitename, client,phone,info FROM orders")
+   result = c.fetchall()
+   c.close()
+   output = template('clients.html', rows=result)
+   return output
+   
+
+#Runner
+run(host='localhost',port=5150,debug=True,reloader=True)
